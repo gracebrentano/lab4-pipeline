001d71140eee533b2cf8fca7234f967a976d6d10
Henry Garant <henryrgarant@gmail.com>
2020-03-29 12:39:56 -0400

    start with single cycle

--- lc4_pipeline.v
+++ lc4_pipeline.v
@@ -1,4 +1,12 @@
-/* TODO: name and PennKeys of all group members here */
+  
+/* Henry Garant(henrygar)
+ * Grace Brentano(brentano)
+ *
+ * lc4_single.v
+ * Implements a fully bypassed scalar pipelined data path
+ *
+ */
+
 
 `timescale 1ns / 1ps
 
@@ -33,7 +41,115 @@ module lc4_processor
     output wire [7:0]  led_data // Which Zedboard LEDs should be turned on?
     );
    
-   /*** YOUR CODE HERE ***/
+   // By default, assign LEDs to display switch inputs to avoid warnings about
+   // disconnected ports. Feel free to use this for debugging input/output if
+   // you desire.
+   assign led_data = switch_data;
+
+   
+   /* DO NOT MODIFY THIS CODE */
+   // Always execute one instruction each cycle (test_stall will get used in your pipelined processor)
+   assign test_stall = 2'b0; 
+
+   // pc wires attached to the PC register's ports
+   wire [15:0]   pc;      // Current program counter (read out from pc_reg)
+   wire [15:0]   next_pc; // Next program counter (you compute this and feed it into next_pc)
+
+   // Program counter register, starts at 8200h at bootup
+   Nbit_reg #(16, 16'h8200) pc_reg (.in(next_pc), .out(pc), .clk(clk), .we(1'b1), .gwe(gwe), .rst(rst));
+
+   /* END DO NOT MODIFY THIS CODE */
+
+   //insn logic
+   assign test_cur_insn = i_cur_insn;
+
+   //decoder
+   wire [2:0] r1sel, r2sel, wsel;
+   wire r1we, r2we, regfile_we, nzp_we, select_pc_plus_one, is_load, is_store, is_branch, is_control_insn;
+   lc4_decoder d (.insn(i_cur_insn),
+                  .r1sel(r1sel),
+                  .r1re(r1we),
+                  .r2sel(r2sel),
+                  .r2re(r2we),
+                  .wsel(wsel),
+                  .regfile_we(regfile_we),
+                  .nzp_we(nzp_we),
+                  .select_pc_plus_one(select_pc_plus_one),
+                  .is_load(is_load),
+                  .is_store(is_store),
+                  .is_branch(is_branch),
+                  .is_control_insn(is_control_insn));
+   assign test_regfile_we = regfile_we;
+   assign test_regfile_wsel = wsel;
+   assign test_nzp_we = nzp_we;
+
+   //make a register
+   wire [15:0] reg_rs_out, reg_rt_out, reg_rd_out;
+   lc4_regfile regFile(.clk(clk),
+                       .gwe(gwe),
+                       .rst(rst),
+                       .i_rs(r1sel),
+                       .o_rs_data(reg_rs_out),
+                       .i_rt(r2sel),
+                       .o_rt_data(reg_rt_out),
+                       .i_rd(wsel),
+                       .i_wdata(test_regfile_data),
+                       .i_rd_we(regfile_we));
+
+    //make the alu jawn
+    wire [15:0] alu_out;
+    lc4_alu alu (.i_insn(i_cur_insn),
+                 .i_pc(pc),
+                 .i_r1data(reg_rs_out),
+                 .i_r2data(reg_rt_out),
+                 .o_result(alu_out));
+
+    //LOAD + STORE
+    wire [15:0] temp_dmem_addr, temp_dmem_data;
+    assign o_dmem_we = i_cur_insn[15:12] == 4'b0111 ? 1'b1 : 1'b0;
+    assign test_dmem_we = o_dmem_we;
+    
+    assign temp_dmem_addr = is_load || is_store ? alu_out : 16'b0;
+    assign o_dmem_addr = temp_dmem_addr;
+    assign test_dmem_addr = temp_dmem_addr;
+
+    assign temp_dmem_data = is_load ? i_cur_dmem_data : (is_store ? reg_rt_out : 16'b0);
+    assign test_dmem_data = temp_dmem_data;
+    assign o_dmem_towrite = is_store ? reg_rt_out : 16'b0;
+
+    //extra pc_plus_one for trap
+    wire [15:0] pc_plus_one;
+    cla16 pc_cla0 (.a(pc), .b(16'b0), .cin(1'd1), .sum(pc_plus_one));
+    
+    //figure out what needs to be stored on next pc cycle
+    assign test_regfile_data = is_load ? test_dmem_data : (i_cur_insn[15:12] == 4'b1111 || i_cur_insn[15:12] == 4'b0100 ? pc_plus_one : alu_out);
+
+    //BRANCH LOGIC
+    wire [2:0] nzp, nzp_reg_out;
+    assign nzp = $signed(test_regfile_data) < $signed(16'b0) ? 3'b100 : ($signed(test_regfile_data) > $signed(16'b0) ? 3'b001 : 3'b010);
+    assign test_nzp_new_bits = nzp;
+    //nzp register
+    Nbit_reg #(3) nzp_reg (.in(nzp), .out(nzp_reg_out), .clk(clk), .we(nzp_we), .gwe(gwe), .rst(rst));
+    
+    wire should_branch;
+    assign should_branch = (i_cur_insn[11] & nzp_reg_out[2]) | (i_cur_insn[10] & nzp_reg_out[1]) | (i_cur_insn[9] & nzp_reg_out[0]);
+    wire [15:0] pc_offset, imm9, imm11;
+    assign imm9 = {{7{i_cur_insn[8]}}, i_cur_insn[8:0]};
+    assign imm11 = {{5{i_cur_insn[10]}}, i_cur_insn[10:0]};
+    assign pc_offset = (should_branch == 1'b1 && is_branch) ? imm9 : (i_cur_insn[15:11] == 5'b11001 ? imm11 : 16'b0);
+    
+    //NEXT PC LOGIC
+    wire [15:0] next_pc_temp_branch, next_pc_temp_rs, next_pc_temp_rti;
+    cla16 pc_cla (.a(pc), .b(pc_offset), .cin(1'd1), .sum(next_pc_temp_branch));
+
+    //compare to jsrr and jmpr mux
+    assign next_pc_temp_rs =  (i_cur_insn[15:11] == 5'b11000 || i_cur_insn[15:11] == 5'b01000) ? reg_rs_out : next_pc_temp_branch;
+
+    //finally if a trap and jsr use alu output
+    assign next_pc_temp_rti = (i_cur_insn[15:12] == 4'b1111 ||  i_cur_insn[15:11] == 5'b01001) ? alu_out : next_pc_temp_rs;
+    assign next_pc = (i_cur_insn[15:12] == 4'b1000) ? reg_rs_out : next_pc_temp_rti;
+    assign test_cur_pc = pc;
+    assign o_cur_pc = pc;
 
    /* Add $display(...) calls in the always block below to
     * print out debug information at the end of every cycle.
